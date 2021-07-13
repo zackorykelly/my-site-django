7a2b365894944bafcc93ad5255463c330f245b37
Zackory Kelly <zackkelly@live.ca>
2021-07-13 13:49:57 -0400

    Fix key issue

--- my_site/settings.py
+++ my_site/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for my_site project.
 
@@ -10,17 +11,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env = environ.Env()
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sl4z6xo63th(&ge&q-p@y$2dy!6x^7eei5)3u*ie_9n(l_h86c'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
